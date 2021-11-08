926a7840701034e588338963f204a53970256b12
ToshikiKawata <kawatatohsiki@icloud.com>
2021-11-08 22:33:25 +0900

    Initial Commit

---
+++ tax_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tax_app
+//
+//  Created by 川田俊希 on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
